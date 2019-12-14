108c299e247453288674666af81a7f62c272bb9c
Tobias Reusing <tobias@reusing.eu>
2019-12-14 15:44:52 +0100

    2019 day12 part 1

---
+++ 2019/day12/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type vec struct {
+	coords []int
+}
+
+type system struct {
+	moons      []vec
+	velocities []vec
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func (s system) calcEnergy() int {
+	totalEnergy := 0
+	for i := range s.moons {
+		pot := 0
+		for _, p := range s.moons[i].coords {
+			pot += abs(p)
+		}
+		kin := 0
+		for _, p := range s.velocities[i].coords {
+			kin += abs(p)
+		}
+		totalEnergy += pot * kin
+	}
+	return totalEnergy
+}
+
+func (s system) calcStep() {
+	for i := range s.moons {
+		for j := i + 1; j < len(s.moons); j++ {
+			for k := 0; k < 3; k++ {
+				if s.moons[i].coords[k] > s.moons[j].coords[k] {
+					s.velocities[i].coords[k]--
+					s.velocities[j].coords[k]++
+				} else if s.moons[i].coords[k] < s.moons[j].coords[k] {
+					s.velocities[i].coords[k]++
+					s.velocities[j].coords[k]--
+				}
+			}
+		}
+	}
+
+	for i := range s.moons {
+		for k := 0; k < 3; k++ {
+			s.moons[i].coords[k] += s.velocities[i].coords[k]
+		}
+	}
+}
+
+func createSystem(input []string) system {
+	sys := system{make([]vec, 0), make([]vec, 0)}
+	for _, str := range input {
+		pos := vec{coords: make([]int, 3)}
+		split := strings.Split(strings.Trim(strings.TrimSpace(str), "<>"), ",")
+		for i, val := range split {
+			split2 := strings.Split(strings.TrimSpace(val), "=")
+			number, _ := strconv.Atoi(strings.TrimSpace(split2[1]))
+			pos.coords[i] = number
+		}
+		sys.moons = append(sys.moons, pos)
+		sys.velocities = append(sys.velocities, vec{coords: make([]int, 3)})
+	}
+	return sys
+}
+
+func simulateSystem(input []string, steps int) int {
+	sys := createSystem(input)
+
+	for i := 0; i < steps; i++ {
+		sys.calcStep()
+	}
+	return sys.calcEnergy()
+}
+
+func howManySteps(in []string) int64 {
+	sys := createSystem(in)
+	for i := int64(0); ; i++ {
+		sys.calcStep()
+		if sys.calcEnergy() == 0 {
+			return i
+		}
+	}
+}
+
+func main() {
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	input := make([]string, 0)
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+	fmt.Printf("Result %v\n", simulateSystem(input, 1000))
+	//fmt.Printf("Result2 %v\n", howManySteps(input))
+
+}
---
+++ 2019/day12/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func Test_calcEnergy(t *testing.T) {
+	type args struct {
+		input []string
+		steps int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example1", args{strings.Split(`<x=-1, y=0, z=2>
+			<x=2, y=-10, z=-7>
+			<x=4, y=-8, z=8>
+			<x=3, y=5, z=-1>`, "\n"), 10}, 179},
+		{"Example2", args{strings.Split(`<x=-8, y=-10, z=0>
+			<x=5, y=5, z=10>
+			<x=2, y=-7, z=3>
+			<x=9, y=-8, z=-3>`, "\n"), 100}, 1940},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := simulateSystem(tt.args.input, tt.args.steps); got != tt.want {
+				t.Errorf("simulateSystem() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
