7b6346d9f4db0202dd1a20681fb0cad657c25d83
Tobias Reusing <tobias@reusing.eu>
2022-12-05 08:39:34 +0100

    2022 day5 unified code for p1 and p2

--- 2022/day05/day05.go
+++ 2022/day05/day05.go
@@ -22,7 +22,25 @@ func parseStacks(in []string) []stack {
 	return stacks
 }
 
-func puzzle1(input []string) string {
+type doMove func([]stack, int, int, int)
+
+func doMoveP1(stacks []stack, count, to, from int) {
+	lf := len(stacks[from])
+	cratesToMove := stacks[from][lf-count:]
+	for i := len(cratesToMove) - 1; i >= 0; i-- {
+		stacks[to] = append(stacks[to], cratesToMove[i])
+	}
+	stacks[from] = stacks[from][:lf-count]
+}
+
+func doMoveP2(stacks []stack, count, to, from int) {
+	lf := len(stacks[from])
+	cratesToMove := stacks[from][lf-count:]
+	stacks[to] = append(stacks[to], cratesToMove...)
+	stacks[from] = stacks[from][:lf-count]
+}
+
+func puzzle(input []string, fn doMove) string {
 	splitInput := gohelpers.SplitByEmptyLine(input)
 	stacks := parseStacks(splitInput[0])
 	moves := splitInput[1]
@@ -32,14 +50,7 @@ func puzzle1(input []string) string {
 		from := 0
 		to := 0
 		fmt.Sscanf(move, "move %d from %d to %d", &count, &from, &to)
-		from--
-		to--
-		lf := len(stacks[from])
-		cratesToMove := stacks[from][lf-count:]
-		for i := len(cratesToMove) - 1; i >= 0; i-- {
-			stacks[to] = append(stacks[to], cratesToMove[i])
-		}
-		stacks[from] = stacks[from][:lf-count]
+		fn(stacks, count, to-1, from-1)
 	}
 
 	result := ""
@@ -49,27 +60,10 @@ func puzzle1(input []string) string {
 	return result
 }
 
+func puzzle1(input []string) string {
+	return puzzle(input, doMoveP1)
+}
+
 func puzzle2(input []string) string {
-	splitInput := gohelpers.SplitByEmptyLine(input)
-	stacks := parseStacks(splitInput[0])
-	moves := splitInput[1]
-
-	for _, move := range moves {
-		count := 0
-		from := 0
-		to := 0
-		fmt.Sscanf(move, "move %d from %d to %d", &count, &from, &to)
-		from--
-		to--
-		lf := len(stacks[from])
-		cratesToMove := stacks[from][lf-count:]
-		stacks[to] = append(stacks[to], cratesToMove...)
-		stacks[from] = stacks[from][:lf-count]
-	}
-
-	result := ""
-	for _, s := range stacks {
-		result += string(s[len(s)-1])
-	}
-	return result
+	return puzzle(input, doMoveP2)
 }
